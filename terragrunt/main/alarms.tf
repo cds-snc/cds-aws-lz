9ff697b4d48020e87d91729ee34cc4e1a3b72d07
CalvinRodo <calvin.rodo@cds-snc.ca>
2022-03-03 11:13:52 -0500

    feat: add a login alarm for ops1 and ops2
    - Creates two sns topics one warn and one critical
    - Alerts when users try to login, alerts on the warning topic when 2
      failures occur, alert on critical when a success occurs

---
+++ terragrunt/main/alarms.tf
@@ -0,0 +1,16 @@
+resource "aws_sns_topic" "critical" { 
+  name = "critical-issue"
+}
+
+resource "aws_sns_topic" "warning" {
+  name = "warning-issue"
+}
+
+module "alarm_actions" {
+
+  account_names = ["Ops1", "Ops2"]
+  log_group_name = "aws-controltower/CloudTrailLogs"
+  alarm_actions_success = [aws_sns_topic.crtical.arn]
+  alarm_actions_failure = [aws_sns_topic.warning.arn]
+  num_attempts = 2
+}
