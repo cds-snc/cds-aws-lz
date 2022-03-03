b3b45aee4e8c4c094707afd9fe1158a47e11d8c3
CalvinRodo <calvin.rodo@cds-snc.ca>
2022-03-03 11:24:39 -0500

    fix: reference to sns topic

--- terragrunt/main/alarms.tf
+++ terragrunt/main/alarms.tf
@@ -10,7 +10,7 @@ module "alarm_actions" {
   source                = "github.com/cds-snc/terraform-modules?ref=v1.0.11//user_login_alarm"
   account_names         = ["Ops1", "Ops2"]
   log_group_name        = "aws-controltower/CloudTrailLogs"
-  alarm_actions_success = [aws_sns_topic.crtical.arn]
+  alarm_actions_success = [aws_sns_topic.critical.arn]
   alarm_actions_failure = [aws_sns_topic.warning.arn]
   num_attempts          = 2
 }
