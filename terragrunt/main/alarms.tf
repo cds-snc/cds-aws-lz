793b817b2d7dc111496b7e434bc381769e360105
CalvinRodo <calvin.rodo@cds-snc.ca>
2022-03-03 11:23:17 -0500

    chore: autofmt

--- terragrunt/main/alarms.tf
+++ terragrunt/main/alarms.tf
@@ -1,4 +1,4 @@
-resource "aws_sns_topic" "critical" { 
+resource "aws_sns_topic" "critical" {
   name = "critical-issue"
 }
 
@@ -7,10 +7,10 @@ resource "aws_sns_topic" "warning" {
 }
 
 module "alarm_actions" {
-  source = "github.com/cds-snc/terraform-modules?ref=v1.0.11//user_login_alarm"
-  account_names = ["Ops1", "Ops2"]
-  log_group_name = "aws-controltower/CloudTrailLogs"
+  source                = "github.com/cds-snc/terraform-modules?ref=v1.0.11//user_login_alarm"
+  account_names         = ["Ops1", "Ops2"]
+  log_group_name        = "aws-controltower/CloudTrailLogs"
   alarm_actions_success = [aws_sns_topic.crtical.arn]
   alarm_actions_failure = [aws_sns_topic.warning.arn]
-  num_attempts = 2
+  num_attempts          = 2
 }
