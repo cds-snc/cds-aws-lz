f270fb2da2512d5be78f93e6a3bd17285733914c
Calvin Rodo <calvin.rodo@cds-snc.ca>
2022-03-04 14:30:26 -0500

    feat: add GoC Pword policy (#4)
    * feat: add GoC Pword policy
    
    - Add a module so we can have a single source of truth
    - Enforce the Password Policy on the Org Level Account
    - Remove the auto added files
    - Add auto-generated files to ignore
    
    * fix: setting pwords to never expire
    
    * chore: autofmt
    
    * fix: remove prop we can't set

---
+++ terragrunt/modules/password_policy/main.tf
@@ -0,0 +1,13 @@
+# This password policy should adhere to the GoC Recommendation for passwords
+# Source: https://www.canada.ca/en/government/system/digital-government/online-security-privacy/password-guidance.html
+
+resource "aws_iam_account_password_policy" "goc" {
+  minimum_password_length        = 12
+  require_symbols                = true
+  require_numbers                = true
+  require_lowercase_characters   = true
+  require_uppercase_characters   = true
+  allow_users_to_change_password = true
+  hard_expiry                    = false
+  password_reuse_prevention      = 24
+}
