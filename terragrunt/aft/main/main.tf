88a7f433d3618a2bbb50dee5a1206ebcbf08dd16
CalvinRodo <calvin.rodo@cds-snc.ca>
2022-03-09 16:33:56 -0500

    fix: fmt and spelling

--- terragrunt/aft/main/main.tf
+++ terragrunt/aft/main/main.tf
@@ -8,23 +8,23 @@ module "account_factory_for_terraform" {
 
   terraform_version = "1.1.6"
 
-  vcs_provider = "github"
-  account_customizations_repo_name = "cds-snc/aft-account-customizations"
+  vcs_provider                                  = "github"
+  account_customizations_repo_name              = "cds-snc/aft-account-customizations"
   account_provisioning_customizations_repo_name = "cds-snc/aft-account-provisioning-customizations"
-  account_request_repo_name = "cds-snc/aft-account-request"
-  global_customizations_repo_name = "cds-snc/aft-global-customizations"
+  account_request_repo_name                     = "cds-snc/aft-account-request"
+  global_customizations_repo_name               = "cds-snc/aft-global-customizations"
 
-  ct_home_region = var.region
+  ct_home_region              = var.region
   tf_backend_secondary_region = "us-east-1"
 
   aft_management_account_id = var.account_id
-  audit_account_id = "886481071419"
-  ct_management_account_id = "659087519042"
-  log_archive_account_id = "274536870005"
+  audit_account_id          = "886481071419"
+  ct_management_account_id  = "659087519042"
+  log_archive_account_id    = "274536870005"
 
-  aft_feature_cloudtrail_data_events = true
-  aft_feautre_delete_default_vpcs_enabled = true
-  aft_feature_enterprise_support = false
-  cloudwatch_log_group_retention = 90
+  aft_feature_cloudtrail_data_events      = true
+  aft_feature_delete_default_vpcs_enabled = true
+  aft_feature_enterprise_support          = false
+  cloudwatch_log_group_retention          = 90
 
 }
