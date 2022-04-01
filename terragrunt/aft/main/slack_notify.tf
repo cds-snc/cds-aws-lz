8749bd2f8d5812eac2bac4adc0ab5d0413d40199
CalvinRodo <calvin.rodo@cds-snc.ca>
2022-04-01 14:10:18 -0400

    feat: notify in slack on AFT notifications
    This should notify in the landing zone slack channel on messages sent to
    aft-failure-notifications and aft-notifications.

---
+++ terragrunt/aft/main/slack_notify.tf
@@ -0,0 +1,24 @@
+module "aft_slack_notification" {
+  source            = "github.com/cds-snc/terraform-modules?ref=v2.0.0//notify_slack"
+  billing_tag_value = var.billing_code
+  function_name     = "aft_slack_notification"
+  project_name      = "Account Factory for Terraform"
+  slack_webhook_url = var.aft_slack_webhook
+
+  sns_topic_arns = [
+    data.aws_sns_topic.aft_failure_notifications.arn,
+    data.aws_sns_topic.aft_notifications.arn
+  ]
+}
+
+data "aws_sns_topic" "aft_failure_notifications" {
+  name = "aft-failure-notifications"
+}
+
+data "aws_sns_topic" "aft_notifications" {
+  name = "aft-notifications"
+}
+
+variable "aft_slack_webhook" {
+  description = "The slack webhook URL to be used by Account Factory for Terraform"
+}
