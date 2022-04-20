1c2d76697c785573e40d76ac49e377682d354797
CalvinRodo <calvin.rodo@cds-snc.ca>
2022-04-20 16:05:12 -0400

    feat: add OIDC to Log Archive Account
    Removes the Sentinel Connectors OIDC for the audit account as we don't
    need them.
    
    Adds them instead to the log archive which is where the GD findings go.

--- terragrunt/log_archive/main/main.tf
+++ terragrunt/log_archive/main/main.tf
@@ -19,6 +19,16 @@ module "gh_oidc_roles" {
       name      = local.admin_name
       repo_name = "cds-aws-lz"
       claim     = "ref:refs/heads/main"
+    },
+    {
+      name      = local.sc_plan_name
+      repo_name = "sentinel-connectors"
+      claim     = "*"
+    },
+    {
+      name      = local.sc_admin_name
+      repo_name = "sentinel-connectors"
+      claim     = "ref:refs/heads/main"
     }
   ]
   oidc_exists       = false
@@ -38,6 +48,19 @@ module "attach_tf_plan_policy" {
   ]
 }
 
+module "attach_tf_plan_policy_sc" {
+  source            = "github.com/cds-snc/terraform-modules?ref=v2.0.2//attach_tf_plan_policy"
+  account_id        = data.aws_caller_identity.current.account_id
+  role_name         = local.sc_plan_name
+  bucket_name       = "${var.billing_code}-tf"
+  lock_table_name   = "terraform-state-lock-dynamo"
+  billing_tag_value = var.billing_code
+  policy_name       = "SCCDSLZTFPlan"
+  depends_on = [
+    module.gh_oidc_roles
+  ]
+}
+
 data "aws_iam_policy" "admin" {
   name = "AdministratorAccess"
   depends_on = [
@@ -47,6 +70,14 @@ data "aws_iam_policy" "admin" {
 
 resource "aws_iam_role_policy_attachment" "admin" {
   role       = local.admin_name
+  policy_arn = data.aws_iam_policy.admin.arn
+  depends_on = [
+    module.gh_oidc_roles
+  ]
+}
+
+resource "aws_iam_role_policy_attachment" "sc_admin" {
+  role       = local.sc_admin_name
   policy_arn = data.aws_iam_policy.admin.arn
   depends_on = [
     module.gh_oidc_roles
