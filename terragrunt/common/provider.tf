74989fbc633f74d8139b5c487c7e6f98d64b17c2
CalvinRodo <calvin.rodo@cds-snc.ca>
2022-04-27 11:14:42 -0400

    feat: set LA as admin account for guardduty

--- terragrunt/common/provider.tf
+++ terragrunt/common/provider.tf
@@ -17,3 +17,9 @@ provider "aws" {
   region              = "us-east-1"
   allowed_account_ids = [var.account_id]
 }
+
+provider "aws" {
+  alias               = "us-west-2"
+  region              = "us-west-2"
+  allowed_account_ids = [var.account_id]
+}
---
+++ terragrunt/org_account/main/guardduty.tf
@@ -0,0 +1,16 @@
+resource "aws_guardduty_organization_admin_account" "gd_admin_ca_central_1" {
+  depends_on       = [aws_organizations_organization.org_config]
+  admin_account_id = "274536870005"
+}
+
+resource "aws_guardduty_organization_admin_account" "gd_admin_us_east_1" {
+  provider         = aws.us-east-1
+  depends_on       = [aws_organizations_organization.org_config]
+  admin_account_id = "274536870005"
+}
+
+resource "aws_guardduty_organization_admin_account" "gd_admin_us_west_2" {
+  provider         = aws.us-west-2
+  depends_on       = [aws_organizations_organization.org_config]
+  admin_account_id = "274536870005"
+}
