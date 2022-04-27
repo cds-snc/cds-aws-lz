a44d5afcaf2871ac19f96932b49760741af5809f
CalvinRodo <calvin.rodo@cds-snc.ca>
2022-04-27 11:29:27 -0400

    feat: dry admin account id

--- terragrunt/org_account/main/guardduty.tf
+++ terragrunt/org_account/main/guardduty.tf
@@ -1,16 +1,21 @@
+locals {
+  admin_account = "274536870005"
+}
+
+
 resource "aws_guardduty_organization_admin_account" "gd_admin_ca_central_1" {
   depends_on       = [aws_organizations_organization.org_config]
-  admin_account_id = "274536870005"
+  admin_account_id = local.admin_account
 }
 
 resource "aws_guardduty_organization_admin_account" "gd_admin_us_east_1" {
   provider         = aws.us-east-1
   depends_on       = [aws_organizations_organization.org_config]
-  admin_account_id = "274536870005"
+  admin_account_id = local.admin_account
 }
 
 resource "aws_guardduty_organization_admin_account" "gd_admin_us_west_2" {
   provider         = aws.us-west-2
   depends_on       = [aws_organizations_organization.org_config]
-  admin_account_id = "274536870005"
+  admin_account_id = local.admin_account
 }
