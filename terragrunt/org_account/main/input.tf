59f6070ba53744b280000ddc00335e533562279b
CalvinRodo <calvin.rodo@cds-snc.ca>
2022-06-06 16:15:20 -0400

    chore: autofmt

--- terragrunt/org_account/main/input.tf
+++ terragrunt/org_account/main/input.tf
@@ -4,11 +4,11 @@ variable "assume_role_name" {
 }
 
 variable "lw_customer_id" {
-  type = string
+  type        = string
   description = "The log workspace customer id for the sentinel forwarder"
 }
 
 variable "lw_shared_key" {
-  type = string
+  type        = string
   description = "The log workspace shared key for the sentinel forwarder"
 }
--- terragrunt/org_account/main/sentinel_forwarders.tf
+++ terragrunt/org_account/main/sentinel_forwarders.tf
@@ -1,17 +1,17 @@
 module "guardduty_forwarder" {
-  source = "github.com/cds-snc/terraform-modules?ref=v2.0.5//sentinel_forwarder"
-  function_name = "senting-guard-duty-forwarder"
+  source            = "github.com/cds-snc/terraform-modules?ref=v2.0.5//sentinel_forwarder"
+  function_name     = "senting-guard-duty-forwarder"
   billing_tag_value = var.billing_code
 
   customer_id = var.lw_customer_id
-  shared_key = var.lw_shared_key
+  shared_key  = var.lw_shared_key
 
   s3_sources = [
     {
-      bucket_arn = module.publishing_bucket.s3_bucket_arn
-      bucket_id = module.publishing_bucket.s3_bucket_id
+      bucket_arn    = module.publishing_bucket.s3_bucket_arn
+      bucket_id     = module.publishing_bucket.s3_bucket_id
       filter_prefix = "AWSLogs/"
-      kms_key_arn = aws_kms_key.cds_sentinel_guard_duty_key.arn
+      kms_key_arn   = aws_kms_key.cds_sentinel_guard_duty_key.arn
     }
   ]
 }
