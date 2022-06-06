2538daf9305947b76cb8a7ffaa75fe6e656c9d01
CalvinRodo <calvin.rodo@cds-snc.ca>
2022-06-06 16:11:16 -0400

    feat: enable guardduty sentinel forwarding
    This uses the terraform module that is already in use by the ALZ org
    account

--- terragrunt/org_account/main/input.tf
+++ terragrunt/org_account/main/input.tf
@@ -2,3 +2,13 @@ variable "assume_role_name" {
   type        = string
   description = "The name of the role to assume"
 }
+
+variable "lw_customer_id" {
+  type = string
+  description = "The log workspace customer id for the sentinel forwarder"
+}
+
+variable "lw_shared_key" {
+  type = string
+  description = "The log workspace shared key for the sentinel forwarder"
+}
