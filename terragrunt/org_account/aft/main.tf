6a8aa144c0a5c4bc01a47857508ad4a29a905145
CalvinRodo <calvin.rodo@cds-snc.ca>
2022-06-30 15:24:12 -0400

    feat: upgrade AFT to 1.5.1

--- terragrunt/org_account/aft/main.tf
+++ terragrunt/org_account/aft/main.tf
@@ -1,5 +1,5 @@
 module "account_factory_for_terraform" {
-  source = "github.com/aws-ia/terraform-aws-control_tower_account_factory?ref=1.4.2"
+  source = "github.com/aws-ia/terraform-aws-control_tower_account_factory?ref=1.5.1"
 
   terraform_version = "1.1.7"
 
