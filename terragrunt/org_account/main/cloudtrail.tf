38cd454a77651d7e9d0cae0b9cce6bcd1d9b3e6a
CalvinRodo <calvin.rodo@cds-snc.ca>
2022-08-10 15:00:09 -0400

    chore: autofmt

--- terragrunt/org_account/main/cloudtrail.tf
+++ terragrunt/org_account/main/cloudtrail.tf
@@ -1,18 +1,18 @@
-data "aws_s3_bucket" "org_logging" { 
+data "aws_s3_bucket" "org_logging" {
   provider = aws.log_archive
 
   bucket = "aws-controltower-logs-274536870005-ca-central-1"
 }
 
 
-data "aws_iam_policy_document" "rep_role" { 
+data "aws_iam_policy_document" "rep_role" {
   statement {
     effect = "Allow"
     principals {
-      type = "Service"
+      type        = "Service"
       identifiers = ["s3.amazonaws.com"]
     }
-    actions = [ "sts:AssumeRole" ]
+    actions = ["sts:AssumeRole"]
 
   }
 }
@@ -28,23 +28,23 @@ resource "aws_iam_role_policy_attachment" "replication" {
   policy_arn = aws_iam_policy.replication.arn
 }
 
-resource "aws_iam_policy" "replication" { 
-  name = "ct-cbs-replication-policy"
+resource "aws_iam_policy" "replication" {
+  name   = "ct-cbs-replication-policy"
   policy = data.aws_iam_policy_document.ct_replication.json
 }
 
 data "aws_iam_policy_document" "ct_replication" {
 
   statement {
-    effect = "Allow"
+    effect    = "Allow"
     resources = [data.aws_s3_bucket.org_logging.arn]
     actions = [
-      "s3:GetReplicationConfiguration", 
+      "s3:GetReplicationConfiguration",
       "s3:ListBucket"
     ]
   }
 
-  statement { 
+  statement {
     effect = "Allow"
     actions = [
       "s3:GetObjectVersionForReplication",
@@ -57,27 +57,27 @@ data "aws_iam_policy_document" "ct_replication" {
   statement {
 
     actions = [
-        "s3:ReplicateObject",
-        "s3:ReplicateDelete",
-        "s3:ReplicateTags"
+      "s3:ReplicateObject",
+      "s3:ReplicateDelete",
+      "s3:ReplicateTags"
     ]
 
     resource = ["${local.destination_bucket_arn}/*"]
   }
 }
 
-resource "aws_s3_bucket_replication_configuration" "replication" { 
+resource "aws_s3_bucket_replication_configuration" "replication" {
   provider = aws.log_archive
 
   bucket = data.aws_s3_bucket.org_logging.id
 
-  rule { 
+  rule {
     id = "cbs_cloudtrail_logs"
 
     destination {
       access_control_translation = "Destination"
-      bucket = local.destination_bucket_arn
-      account_id = local.destination_account_id
+      bucket                     = local.destination_bucket_arn
+      account_id                 = local.destination_account_id
       encryption_configuration {
         replica_kms_key_id = local.destination_kms_key_id
       }
@@ -91,8 +91,8 @@ resource "aws_s3_bucket_replication_configuration" "replication" {
 }
 
 
-locals { 
-  destination_bucket_arn = "arn:aws:s3:::cbs-log-archive-871282759583"
+locals {
+  destination_bucket_arn  = "arn:aws:s3:::cbs-log-archive-871282759583"
   destination_kms_key_arn = "arn:aws:kms:ca-central-1:871282759583:key/c4591f87-9445-4840-acb6-a5569e703c93"
-  destination_account_id = ""
+  destination_account_id  = ""
 }
