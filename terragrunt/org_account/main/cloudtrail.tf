f4e676c6aa06fd5569456aa8c9592ec35557285e
CalvinRodo <calvin.rodo@cds-snc.ca>
2022-08-11 10:18:46 -0400

    fix: add delete_marker_replication

--- terragrunt/org_account/main/cloudtrail.tf
+++ terragrunt/org_account/main/cloudtrail.tf
@@ -86,9 +86,15 @@ resource "aws_s3_bucket_replication_configuration" "replication" {
     destination {
       bucket  = local.destination_bucket_arn
       account = local.destination_account_id
+
+      delete_marker_replication {
+        status = "Enabled"
+      }
+
       encryption_configuration {
         replica_kms_key_id = local.destination_kms_key_arn
       }
+
       access_control_translation {
         owner = "Destination"
       }
