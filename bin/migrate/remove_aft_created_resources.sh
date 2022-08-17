67519b97fca5675fd6736fb8f11b0e8797a5dc7d
CalvinRodo <calvin.rodo@cds-snc.ca>
2022-08-17 13:46:35 -0400

    feat: adds migration scripts
    Adds the following scripts that are used during account migration.
    
    - bin/migrate/create_awsctexec_role.sh
      Creates the AWSControlTowerExecution role that's required by control
    tower for creating resources in the account
    
    - bin/migrate/remove_aft_created_resources.sh
      Deletes resources that are created by AFT, this should fail for some
    lines of these script as some resources are only used by some environments
    
    - bin/migrate/invoke_account.sh
      Invokes the AFTPipeline for a specific account

---
+++ bin/migrate/remove_aft_created_resources.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
+
+echo "Removing resources in account $ACCOUNT_ID"
+echo "Type REMOVE to confirm"
+read -r RESPONSE
+
+if [ "$RESPONSE" == "REMOVE" ]; then
+  echo "Removing resources in account $ACCOUNT_ID"
+
+  echo "Removing Github OIDC Provider"
+  aws iam delete-open-id-connect-provider --open-id-connect-provider-arn "arn:aws:iam::$ACCOUNT_ID:oidc-provider/token.actions.githubusercontent.com"
+
+  echo "Removing secopsAssetInventorySecurityAuditRole"
+  aws iam detach-role-policy --policy-arn arn:aws:iam::aws:policy/SecurityAudit --role-name secopsAssetInventorySecurityAuditRole
+  aws iam delete-role --role-name secopsAssetInventorySecurityAuditRole
+
+  echo "Removing ConfigTerraformAdminExecutionRole"
+  aws iam detach-role-policy --policy-arn arn:aws:iam::aws:policy/AdministratorAccess --role-name ConfigTerraformAdminExecutionRole
+  aws iam delete-role --role-name ConfigTerraformAdminExecutionRole
+
+  echo "Removing CbsSatelliteReplicateToLogArchive"
+  aws iam detach-role-policy --policy-arn arn:aws:iam::806545929748:policy/CbsSatelliteReplicateToLogArchive --role-name CbsSatelliteReplicateToLogArchive
+  aws iam delete-role --role-name CbsSatelliteReplicateToLogArchive
+  aws iam delete-policy --policy-arn arn:aws:iam::806545929748:policy/CbsSatelliteReplicateToLogArchive
+
+else
+  echo "Aborting"
+fi
