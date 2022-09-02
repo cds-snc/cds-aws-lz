df1c11ed47704884f98f6dc9c16c2aac70311ca6
Guillaume Charest <guillaume.charest@cds-snc.ca>
2022-09-02 18:52:16 +0000

    feat: Add one off fix for AdminAccess policy

---
+++ bin/one-offs/add_admin_policy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ACCOUNT_LIST="$(aws configure list-profiles)"
+
+
+while IFS= read -r AWS_PROFILE
+do
+
+  if [[ "$AWS_PROFILE" != *".AdministratorAccess"* ]]; then
+    continue
+  fi
+
+    echo "Onboard account: $AWS_PROFILE"
+    ASSUMED_ROLE="$(aws --profile "$AWS_PROFILE" sts get-caller-identity | jq -r '.Arn')"
+
+    echo "Assumed role via SSO: $ASSUMED_ROLE"
+
+    echo "Attaching AdministratorAccess to group admins"
+    aws --profile "$AWS_PROFILE" iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AdministratorAccess --group-name admins
+
+    sleep 1
+
+done < <(printf'%s\n' "$ACCOUNT_LIST")
