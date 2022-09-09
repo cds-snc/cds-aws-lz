ad5005cf9b5e2db0dd49f22476b682addf05f14a
CalvinRodo <calvin.rodo@cds-snc.ca>
2022-09-09 16:22:24 -0400

    fix: issue with caller identity

--- terragrunt/org_account/main/spend_notifier.tf
+++ terragrunt/org_account/main/spend_notifier.tf
@@ -23,7 +23,7 @@ data "aws_iam_policy_document" "spend_notifier" {
       "logs:PutLogEvents"
     ]
     resources = [
-      "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity_current.account_id}:log-group:/aws/lambda/daily-spend-monitor:*"
+      "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/daily-spend-monitor:*"
     ]
 
   }
