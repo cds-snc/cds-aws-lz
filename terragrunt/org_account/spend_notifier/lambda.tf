9d1fbac8019924e47ed778b71d8bb648260eebd6
Calvin Rodo <calvin.rodo@cds-snc.ca>
2022-09-14 15:30:40 -0400

    fix: add log group and lambda insights (#106)
    * fix: add log group and lambda insights
    
    Attach the policy for lambda insights.
    Create a log group so we can figure out why it failed to execute from
    eventbridge.

--- terragrunt/org_account/spend_notifier/lambda.tf
+++ terragrunt/org_account/spend_notifier/lambda.tf
@@ -43,3 +43,9 @@ resource "aws_lambda_permission" "allow_weekly_budget" {
 }
 
 
+resource "aws_cloudwatch_log_group" "this" {
+  #checkov:skip=CKV_AWS_158:We trust the AWS provided keys
+  name              = "/aws/lambda/${aws_lambda_function.spend_notifier.function_name}"
+  retention_in_days = "14"
+  tags              = local.common_tags
+}
