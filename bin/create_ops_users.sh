cd882ea7c2f4ca1f0a08d4b739b929782c8f3982
Guillaume Charest <guillaume.charest@cds-snc.ca>
2022-09-21 09:59:08 -0400

    feat: Add lpass status evaluation

--- bin/create_ops_users.sh
+++ bin/create_ops_users.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
 
-ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
-aws iam create-group --group-name admins
-aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AdministratorAccess --group-name admins
+LPASS_STATUS="$(lpass status)"
 
-aws iam create-user --user-name ops1
-aws iam add-user-to-group --user-name ops1 --group-name admins
-OPS1PWORD=$(aws secretsmanager get-random-password | jq -r '.RandomPassword')
-aws iam create-login-profile --user-name ops1 --password "$OPS1PWORD"
+if [[ "$LPASS_STATUS" =~ ^(Logged in as) ]]
+then
+  ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
+  
+  aws iam create-group --group-name admins
+  aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AdministratorAccess --group-name admins
 
-aws iam create-user --user-name ops2
-aws iam add-user-to-group --user-name ops2 --group-name admins
-OPS2PWORD=$(aws secretsmanager get-random-password | jq -r '.RandomPassword')
-aws iam create-login-profile --user-name ops2 --password "$OPS2PWORD"
+  aws iam create-user --user-name ops1
+  aws iam add-user-to-group --user-name ops1 --group-name admins
+  OPS1PWORD=$(aws secretsmanager get-random-password | jq -r '.RandomPassword')
+  aws iam create-login-profile --user-name ops1 --password "$OPS1PWORD"
 
-lpass add --notes "Shared-SRE - AWS Cloud credentials/$ACCOUNT_ID" --non-interactive --sync=now <<EOF
+  aws iam create-user --user-name ops2
+  aws iam add-user-to-group --user-name ops2 --group-name admins
+  OPS2PWORD=$(aws secretsmanager get-random-password | jq -r '.RandomPassword')
+  aws iam create-login-profile --user-name ops2 --password "$OPS2PWORD"
 
-uname: ops1
-pword: $OPS1PWORD
+  lpass add --notes "Shared-SRE - AWS Cloud credentials/$ACCOUNT_ID" --non-interactive --sync=now <<EOF
 
-uname: ops2
-pword: $OPS2PWORD
+  uname: ops1
+  pword: $OPS1PWORD
+
+  uname: ops2
+  pword: $OPS2PWORD
 
 EOF
+
+else
+  echo  "Error: Must be logged in lpass"
+fi
