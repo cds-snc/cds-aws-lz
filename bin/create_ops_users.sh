0c30dc75e79215ab2bfa0553f0a354c0779fc6da
Guillaume Charest <guillaume.charest@cds-snc.ca>
2022-09-21 10:33:05 -0400

    fix: use guard clause

--- bin/create_ops_users.sh
+++ bin/create_ops_users.sh
@@ -1,34 +1,32 @@
 #!/bin/bash
 
-LPASS_STATUS="$(lpass status)"
+lpass status
+if [ $? -ne 0 ]; then 
+  echo "Error: Must be logged in lpass"
+  exit 1
+fi
 
-if [[ "$LPASS_STATUS" =~ ^(Logged in as) ]]
-then
-  ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
-  
-  aws iam create-group --group-name admins
-  aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AdministratorAccess --group-name admins
+ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
 
-  aws iam create-user --user-name ops1
-  aws iam add-user-to-group --user-name ops1 --group-name admins
-  OPS1PWORD=$(aws secretsmanager get-random-password | jq -r '.RandomPassword')
-  aws iam create-login-profile --user-name ops1 --password "$OPS1PWORD"
+aws iam create-group --group-name admins
+aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AdministratorAccess --group-name admins
 
-  aws iam create-user --user-name ops2
-  aws iam add-user-to-group --user-name ops2 --group-name admins
-  OPS2PWORD=$(aws secretsmanager get-random-password | jq -r '.RandomPassword')
-  aws iam create-login-profile --user-name ops2 --password "$OPS2PWORD"
+aws iam create-user --user-name ops1
+aws iam add-user-to-group --user-name ops1 --group-name admins
+OPS1PWORD=$(aws secretsmanager get-random-password | jq -r '.RandomPassword')
+aws iam create-login-profile --user-name ops1 --password "$OPS1PWORD"
 
-  lpass add --notes "Shared-SRE - AWS Cloud credentials/$ACCOUNT_ID" --non-interactive --sync=now <<EOF
+aws iam create-user --user-name ops2
+aws iam add-user-to-group --user-name ops2 --group-name admins
+OPS2PWORD=$(aws secretsmanager get-random-password | jq -r '.RandomPassword')
+aws iam create-login-profile --user-name ops2 --password "$OPS2PWORD"
 
-  uname: ops1
-  pword: $OPS1PWORD
+lpass add --notes "Shared-SRE - AWS Cloud credentials/$ACCOUNT_ID" --non-interactive --sync=now <<EOF
 
-  uname: ops2
-  pword: $OPS2PWORD
+uname: ops1
+pword: $OPS1PWORD
+
+uname: ops2
+pword: $OPS2PWORD
 
 EOF
-
-else
-  echo  "Error: Must be logged in lpass"
-fi
