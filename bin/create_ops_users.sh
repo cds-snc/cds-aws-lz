7c63638199ff385e448cbbb68814c5a7be4bb6d8
Pat Heard <patrick.heard@cds-snc.ca>
2022-09-28 16:49:33 -0400

    fix: add `region` arg to AWS cli command (#115)
    The variable I set previously was not passing correctly to the
    subshell created by `$()`.

--- bin/create_ops_users.sh
+++ bin/create_ops_users.sh
@@ -8,20 +8,17 @@ fi
 
 ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
 
-# shellcheck disable=SC2034
-AWS_DEFAULT_REGION="ca-central-1"
-
 aws iam create-group --group-name admins
 aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AdministratorAccess --group-name admins
 
 aws iam create-user --user-name ops1
 aws iam add-user-to-group --user-name ops1 --group-name admins
-OPS1PWORD=$(aws secretsmanager get-random-password | jq -r '.RandomPassword')
+OPS1PWORD=$(aws secretsmanager get-random-password --region ca-central-1 | jq -r '.RandomPassword')
 aws iam create-login-profile --user-name ops1 --password "$OPS1PWORD"
 
 aws iam create-user --user-name ops2
 aws iam add-user-to-group --user-name ops2 --group-name admins
-OPS2PWORD=$(aws secretsmanager get-random-password | jq -r '.RandomPassword')
+OPS2PWORD=$(aws secretsmanager get-random-password --region ca-central-1 | jq -r '.RandomPassword')
 aws iam create-login-profile --user-name ops2 --password "$OPS2PWORD"
 
 lpass add --notes "Shared-SRE - AWS Cloud credentials/Control Tower/$ACCOUNT_ID" --non-interactive --sync=now <<EOF
