df50531a3b19fe0b0eac3282c7a5115d30d1c87c
Guillaume Charest <guillaume.charest@cds-snc.ca>
2022-10-12 15:58:36 -0400

    fix: remove provider

--- terragrunt/log_archive/legacy_archives/main.tf
+++ terragrunt/log_archive/legacy_archives/main.tf
@@ -1,19 +1,11 @@
 module "aws-landing-zone-logs_bucket" {
   source = "github.com/cds-snc/terraform-modules?ref=v3.0.19//S3_log_bucket"
 
-  providers = {
-    aws = aws.log_archive
-  }
-
   billing_tag_value = var.billing_code
 }
 
 module "aws-landing-zone-s3-access-logs_bucket" {
   source = "github.com/cds-snc/terraform-modules?ref=v3.0.19//S3_log_bucket"
-
-  providers = {
-    aws = aws.log_archive
-  }
 
   billing_tag_value = var.billing_code
 }
