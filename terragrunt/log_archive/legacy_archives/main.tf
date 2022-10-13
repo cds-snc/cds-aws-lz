b4511d6ac32678402c25c9911c29fd173d2f9d64
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2022-10-13 10:14:26 -0400

    feat: Create S3 buckets for batch replication (#122)
    * feat: Create S3 buckets for batch replication
    
    * fix: folder structure
    
    * fix: add plan/apply modules
    
    * fix: update module name
    
    * fix: shorten role-session-name
    
    * fix: remove provider
    
    * fix: use base s3 module

---
+++ terragrunt/log_archive/legacy_archives/main.tf
@@ -0,0 +1,11 @@
+module "aws-landing-zone-logs_bucket" {
+  source = "github.com/cds-snc/terraform-modules?ref=v3.0.19//S3"
+
+  billing_tag_value = var.billing_code
+}
+
+module "aws-landing-zone-s3-access-logs_bucket" {
+  source = "github.com/cds-snc/terraform-modules?ref=v3.0.19//S3"
+
+  billing_tag_value = var.billing_code
+}
