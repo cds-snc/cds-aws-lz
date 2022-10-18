c220f6c9ae6978d95d8f109a6798c693bf59d47a
Max Neuvians <max.neuvians@cds-snc.ca>
2022-10-18 12:22:43 +0000

    fix: add KMS key

--- terragrunt/log_archive/legacy_archives/main.tf
+++ terragrunt/log_archive/legacy_archives/main.tf
@@ -3,6 +3,8 @@ module "aws-landing-zone-logs_bucket" {
 
   bucket_name       = "legacy-aws-landing-zone-logs"
   billing_tag_value = var.billing_code
+
+  kms_key_arn = "arn:aws:kms:ca-central-1:${var.account_id}:alias/aws/s3"
 }
 
 module "aws-landing-zone-s3-access-logs_bucket" {
