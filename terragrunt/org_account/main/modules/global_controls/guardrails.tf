b87c81acd83e86c6bf6cbc0b49435a7e7873c471
CalvinRodo <calvin.rodo@cds-snc.ca>
2022-11-25 09:39:02 -0500

    feat: stop trying to be clever

--- terragrunt/org_account/main/modules/global_controls/guardrails.tf
+++ terragrunt/org_account/main/modules/global_controls/guardrails.tf
@@ -1,5 +1,4 @@
 locals {
-
   strongly_recommend_controls = toset(["AWS-GR_ENCRYPTED_VOLUMES",
     "AWS-GR_RDS_INSTANCE_PUBLIC_ACCESS_CHECK",
     "AWS-GR_RDS_SNAPSHOTS_PUBLIC_PROHIBITED",
@@ -11,20 +10,28 @@ locals {
     "AWS-GR_DETECT_CLOUDTRAIL_ENABLED_ON_MEMBER_ACCOUNTS",
     "AWS-GR_RDS_STORAGE_ENCRYPTED"
   ])
-  regions = toset(["ca-central-1", "us-east-1", "us-west-2"])
-
-  src_map    = [ for ctl in local.strongly_recommend_controls : {"control" = ctl }]
-  region_map = [ for region in local.regions : {"region" = region }]
-
-  # Create a list of all possible combinations fo regions and controls
-  controls = toset([ for x in setproduct(local.src_map, local.region_map): merge(x...)])
 }
 
-resource "aws_controltower_control" "_" {
-  for_each = { for control in local.controls: control.control => control}
+resource "aws_controltower_control" "ca_central_1" {
+  for_each = local.strongly_recommend_controls
 
-  control_identifier = "arn:aws:controltower:${each.value.region}::control/${each.value.control}"
+  control_identifier = "arn:aws:controltower:ca-central-1::control/${each.value}"
   target_identifier  = var.ou_arn
 
 }
 
+resource "aws_controltower_control" "us_east_1" {
+  for_each = local.strongly_recommend_controls
+
+  control_identifier = "arn:aws:controltower:us-east-1::control/${each.value}"
+  target_identifier  = var.ou_arn
+
+}
+
+resource "aws_controltower_control" "us_west_2" {
+  for_each = local.strongly_recommend_controls
+
+  control_identifier = "arn:aws:controltower:us-west-2::control/${each.value}"
+  target_identifier  = var.ou_arn
+
+}
