e04893f9aec9d5464d6d71a536ca4f0ccd4ce5c4
CalvinRodo <calvin.rodo@cds-snc.ca>
2022-11-29 15:48:06 -0500

    feat: remove unused roles

--- terragrunt/org_account/main/oidc_role.tf
+++ terragrunt/org_account/main/oidc_role.tf
@@ -21,29 +21,8 @@ module "gh_oidc_roles" {
   billing_tag_value = var.billing_code
 }
 
-module "attach_tf_plan_policy" {
-  source            = "github.com/cds-snc/terraform-modules?ref=v3.0.2//attach_tf_plan_policy"
-  account_id        = data.aws_caller_identity.current.account_id
-  role_name         = local.plan_name
-  bucket_name       = "${var.billing_code}-tf"
-  lock_table_name   = "terraform-state-lock-dynamo"
-  billing_tag_value = var.billing_code
-  policy_name       = "CDSLZTFPlan"
-  depends_on = [
-    module.gh_oidc_roles
-  ]
-}
-
 data "aws_iam_policy" "admin" {
   name = "AdministratorAccess"
-  depends_on = [
-    module.gh_oidc_roles
-  ]
-}
-
-resource "aws_iam_role_policy_attachment" "admin" {
-  role       = local.admin_name
-  policy_arn = data.aws_iam_policy.admin.arn
   depends_on = [
     module.gh_oidc_roles
   ]
