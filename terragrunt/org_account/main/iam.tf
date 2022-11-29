c1e2e3a23e14fe3742c966eb0f08985972c1a1bd
CalvinRodo <calvin.rodo@cds-snc.ca>
2022-11-29 16:28:12 -0500

    chore: autofmt

--- terragrunt/org_account/main/iam.tf
+++ terragrunt/org_account/main/iam.tf
@@ -2,20 +2,20 @@
 
 data "aws_iam_policy_document" "ct_list_controls" {
   statement {
-    effect = "Allow"
-    actions = ["controltower:ListEnabledControls"]
+    effect    = "Allow"
+    actions   = ["controltower:ListEnabledControls"]
     resources = ["*"]
   }
 }
 
 resource "aws_iam_policy" "ct_list_controls" {
-  name = "CDSListControlTowerControls"
+  name        = "CDSListControlTowerControls"
   description = "List Control Tower Controls"
-  policy = data.aws_iam_policy_document.ct_list_controls.json
+  policy      = data.aws_iam_policy_document.ct_list_controls.json
 }
 
 
 resource "aws_iam_role_policy_attachment" "ct_list_controls" {
-  role = local.plan_name
+  role       = local.plan_name
   policy_arn = aws_iam_policy.ct_list_controls.arn
 }
