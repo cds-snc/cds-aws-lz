84b7a68157b7e5b11b9c9eb191a38bb09efdc626
CalvinRodo <calvin.rodo@cds-snc.ca>
2023-01-05 16:18:16 -0500

    fix: typo in module name

--- terragrunt/org_account/roles/OIDC.tf
+++ terragrunt/org_account/roles/OIDC.tf
@@ -3,7 +3,7 @@ locals {
 }
 
 module "OIDC_Roles" {
-  source      = "github.com/cds-snc/terraform-modules?ref=v5.0.0//gh_oicd_role"
+  source      = "github.com/cds-snc/terraform-modules?ref=v5.0.0//gh_oidc_role"
   oidc_exists = true
 
   roles = [{
