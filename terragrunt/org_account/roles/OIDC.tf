cc945d3ee50d8291dbe6755f673a5333ae3cd71e
Calvin Rodo <calvin.rodo@cds-snc.ca>
2023-01-06 12:00:49 -0500

    feat: refactor and add roles (#159)
    * feat: refactor and add roles
    
    - SRE-bot module is now a generic roles module
    - Add an OIDC role to be used by nuke to print out account ids in the
      sandbox ou

---
+++ terragrunt/org_account/roles/OIDC.tf
@@ -0,0 +1,42 @@
+locals {
+  org_account_list_name = "listAccountsInOrg"
+}
+
+module "OIDC_Roles" {
+  source      = "github.com/cds-snc/terraform-modules?ref=v5.0.0//gh_oidc_role"
+  oidc_exists = true
+
+  roles = [{
+    name      = local.org_account_list_name
+    repo_name = "site-reliability-engineering"
+    claim     = "ref:refs/heads/main"
+  }]
+
+  billing_tag_value = var.billing_code
+}
+
+# An IAM policy document that allows you to query all the accounts in an OU
+data "aws_iam_policy_document" "org_account_list_in_sandbox" {
+  statement {
+    sid = "ListAccountsInSandboxOU"
+
+    actions = [
+      "organizations:ListAccountsForParent",
+    ]
+
+    resources = [
+      "arn:aws:organizations::659087519042:ou/o-625no8z3dd/ou-5gsq-qhvjdryl",
+    ]
+  }
+}
+
+resource "aws_iam_policy" "org_account_list_in_sandbox" {
+  name   = local.org_account_list_name
+  policy = data.aws_iam_policy_document.org_account_list_in_sandbox.json
+}
+
+# Attach the policy document to the role loca.org_account_list_name
+resource "aws_iam_role_policy_attachment" "attach_list_accounts_in_sandbox" {
+  role       = local.org_account_list_name
+  policy_arn = aws_iam_policy.org_account_list_in_sandbox.arn
+}
