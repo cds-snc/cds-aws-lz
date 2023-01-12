312cda87d3017bd8b67c4dea0074caaebb62480b
Guillaume Charest <guillaume.charest@cds-snc.ca>
2023-01-12 11:29:05 -0500

    fix: lint and format

--- bin/update_ops_users_passwords.sh
+++ bin/update_ops_users_passwords.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-lpass status
-if [ $? -ne 0 ]; then 
+if ! lpass status; then
   echo "Error: Must be logged in lpass"
   exit 1
 fi
@@ -13,19 +12,18 @@ USERS=$(aws iam list-users | jq .Users | jq '.[] | .UserName')
 echo "Updating passwords for account id: $ACCOUNT_ID"
 
 # Check if ops1 and ops2 users are in the account
-if [[ ! "$USERS" =~ "ops1" && ! "$USERS" =~ "ops2" ]]
-then
+if [[ ! "$USERS" =~ "ops1" && ! "$USERS" =~ "ops2" ]]; then
   echo "Requires users 'ops1' and 'ops2' to be present in the account."
   exit 1
 fi
 
 OPS1PWORD=$(aws secretsmanager get-random-password --region ca-central-1 | jq -r '.RandomPassword')
-aws iam update-login-profile --user-name ops1 --password "$OPS1PWORD" 
+aws iam update-login-profile --user-name ops1 --password "$OPS1PWORD"
 
 OPS2PWORD=$(aws secretsmanager get-random-password --region ca-central-1 | jq -r '.RandomPassword')
 aws iam update-login-profile --user-name ops2 --password "$OPS2PWORD"
 
-lpass add --notes "Shared-SRE - AWS Cloud credentials/Control Tower/$ACCOUNT_ID" --non-interactive --sync=now <<EOF
+lpass add --notes "Shared-SRE - AWS Cloud credentials/Control Tower/$ACCOUNT_ID - Rotated" --non-interactive --sync=now <<EOF
 ~~~~~~
 Updated on: $NOW
 
@@ -34,6 +32,5 @@ pword: $OPS1PWORD
 
 uname: ops2
 pword: $OPS2PWORD
-
 ~~~~~~
 EOF
