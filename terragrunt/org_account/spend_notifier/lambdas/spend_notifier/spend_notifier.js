a072524ba91dca8b9a329c31f97c8f086426207a
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2023-08-21 10:56:18 -0700

    Alert on 35% increases in daily AWS account cost (#199)
    * Committing changes to the spend notifier function to calculate the daily cost increases
    
    * Changing the account percentage increases to 35%
    
    * Removing testing function name
    
    * Removing print statements
    
    * Removing unneeded comments
    
    * Changing alert message
    
    * Removing console log statements

--- terragrunt/org_account/spend_notifier/lambdas/spend_notifier/spend_notifier.js
+++ terragrunt/org_account/spend_notifier/lambdas/spend_notifier/spend_notifier.js
@@ -7,15 +7,24 @@ const https = require('https')
 exports.handler = async (event) => {
   const hook = event.hook;
   const today = new Date();
+
+  // call the daily cost function to get daily costs
+  const dailyAccountCost = await getDailyAccountCost()
+
   const accountCost = await getAccountCost()
   let accounts = await getAccounts()
+  let accountIncreases = {}
   Object.keys(accounts).forEach(key => {
     if(accountCost.hasOwnProperty(key)){
       accounts[key]["Cost"] = accountCost[key]
     } else {
       accounts[key]["Cost"] = 0
     }
-  })
+    // if there is a 35% increase in costs for yesterady vs day before, add to accountIncreases
+    if(dailyAccountCost.hasOwnProperty(key) && dailyAccountCost[key] > 35) {
+          accountIncreases[accounts[key]["Name"]] = dailyAccountCost[key]
+    }
+  });
   let BU = {}
 
   Object.values(accounts).forEach(account => {
@@ -47,9 +56,23 @@ exports.handler = async (event) => {
         "fields": [{ "type": "mrkdwn", "text": `*${bu[0]}*` }, { "type": "mrkdwn", "text": `$${bu[1].toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')} USD` }]
     }
     )).flat()
+
+  // concatenate accounts that have increases and construct a new blocks section 
+  const costIncreasedAccounts= Object.keys(accountIncreases).join(', ').toString()
+  const costIncreasedAccountsSection= {
+        "type": "section",
+        "text": 
+          { "type": "mrkdwn", "text": `Account(s) *${costIncreasedAccounts}* saw at least *35% increase in cost* yesterday from previous day cost calculations.` },
+    }
+
   blocks.splice(0,0, header)
   blocks.push({ "type": "divider"})
   blocks.push(footer)
+
+  // if there are accounts that have 35% increase in cost, add the section to the message
+  if (costIncreasedAccounts.length > 0) {
+    blocks.push(costIncreasedAccountsSection);
+  }
   const data = JSON.stringify(
     {
       "blocks": blocks
@@ -127,6 +150,69 @@ async function getAccountCost() {
     acc[curr["Keys"][0]] = parseFloat(curr["Metrics"]["UnblendedCost"]["Amount"]);
     return acc;
   }, {});
+}
+
+/**
+ * Calculates the daily account cost for yesterday and the day before yesterday, and returns the percentage increase.
+ * @returns {Object} An object containing the percentage increase in cost for each linked account.
+ */
+async function getDailyAccountCost() {
+  const today = new Date();
+  const yesterday = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 1).toISOString().split("T")[0];
+  const dayBeforeYesterday = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 2).toISOString().split("T")[0];
+  const twoDaysBeforeYesterday = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 3).toISOString().split("T")[0];
+
+  // construct params for cost explorer
+  const paramsYesterday = {
+    Granularity: "DAILY",
+    TimePeriod: { Start: dayBeforeYesterday, End: yesterday},
+    Metrics: ["UNBLENDED_COST"],
+    GroupBy: [
+      {
+        Type: "DIMENSION",
+        Key: "LINKED_ACCOUNT"
+      }]
+  };
+
+  const paramsDayBeforeYesterday = {
+    Granularity: "DAILY",
+    TimePeriod: { Start: twoDaysBeforeYesterday, End: dayBeforeYesterday},
+    Metrics: ["UNBLENDED_COST"],
+    GroupBy: [
+      {
+        Type: "DIMENSION",
+        Key: "LINKED_ACCOUNT"
+      }]
+  };
+
+  // get cost for yesterday and the day before yesterday
+  const getCostsYesterday = await costexplorer.getCostAndUsage(paramsYesterday).promise();
+  const getCostsDayBeforeYesterday = await costexplorer.getCostAndUsage(paramsDayBeforeYesterday).promise();
+
+  // get the the amounts from the object for yesterday and the day before yesterday
+  const yesterdayCosts = getCostsYesterday["ResultsByTime"][0]["Groups"].reduce((acc, curr) => {
+    acc[curr["Keys"][0]] = parseFloat(curr["Metrics"]["UnblendedCost"]["Amount"]);
+    return acc;
+  }, {});
+
+  const dayBeforeYesterdayCosts = getCostsDayBeforeYesterday["ResultsByTime"][0]["Groups"].reduce((acc, curr) => {
+    acc[curr["Keys"][0]] = parseFloat(curr["Metrics"]["UnblendedCost"]["Amount"]);
+    return acc;
+  }, {});
+
+  //calculate the difference between yesterday and the day before yesterday
+  const difference = Object.keys(yesterdayCosts).reduce((acc, curr) => {
+    acc[curr] = yesterdayCosts[curr] - dayBeforeYesterdayCosts[curr];
+    return acc;
+  }, {});
+
+  //calculate the percentage increase and return it 
+  const percentageIncrease = Object.keys(difference).reduce((acc, curr) => {
+    acc[curr] = ((difference[curr] / dayBeforeYesterdayCosts[curr]) * 100).toFixed(2);
+    return acc;
+  }, {});
+  
+  return percentageIncrease;
 }
 
 function doRequest(options, data) {
