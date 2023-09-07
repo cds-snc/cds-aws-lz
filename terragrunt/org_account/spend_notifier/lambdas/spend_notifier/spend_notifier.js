6b7570d82506c294f283cbeb428be38c50692da8
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2023-09-07 12:23:18 -0700

    Bug fixes with dates and Aws cost explorer api (#205)
    * Fixing up bugs
    
    * Rewriting the dates of the spend notifier to make it less confusing

--- terragrunt/org_account/spend_notifier/lambdas/spend_notifier/spend_notifier.js
+++ terragrunt/org_account/spend_notifier/lambdas/spend_notifier/spend_notifier.js
@@ -187,14 +187,14 @@ async function getAccountCost() {
  */
 async function getScratchAccountsExceedingThreshold() {
   const today = new Date();
+  const todayDate = today.toISOString().split("T")[0];
   const firstDayOfMonth = new Date(today.getFullYear(), today.getMonth(), 1).toISOString().split("T")[0];
   const yesterday = new Date(today.setDate(today.getDate() - 1)).toISOString().split("T")[0];
-  const dayBeforeYesterday = new Date(today.setDate(today.getDate() - 2)).toISOString().split("T")[0];
 
   // construct params for cost explorer
   const paramsYesterday = {
     Granularity: "MONTHLY",
-    TimePeriod: { Start: firstDayOfMonth, End: yesterday},
+    TimePeriod: { Start: firstDayOfMonth, End: todayDate},
     Metrics: ["UNBLENDED_COST"],
     GroupBy: [
       {
@@ -205,7 +205,7 @@ async function getScratchAccountsExceedingThreshold() {
 
   const paramsDayBeforeYesterday = {
     Granularity: "MONTHLY",
-    TimePeriod: { Start: firstDayOfMonth, End: dayBeforeYesterday},
+    TimePeriod: { Start: firstDayOfMonth, End: yesterday},
     Metrics: ["UNBLENDED_COST"],
     GroupBy: [
       {
@@ -239,8 +239,8 @@ async function getScratchAccountsExceedingThreshold() {
 async function getDailyAccountCost() {
   const today = new Date();
   const dayToday = today.toISOString().split("T")[0];
-  const yesterday = new Date(today.setDate(today.getDate() - 1)).toISOString().split("T")[0];
-  const dayBeforeYesterday = new Date(today.setDate(today.getDate() - 2)).toISOString().split("T")[0];
+  const yesterday = new Date(new Date().setDate(today.getDate() - 1)).toISOString().split("T")[0];
+  const dayBeforeYesterday = new Date(new Date().setDate(today.getDate() - 2)).toISOString().split("T")[0];
 
   // construct params for cost explorer
   const paramsYesterday = {
