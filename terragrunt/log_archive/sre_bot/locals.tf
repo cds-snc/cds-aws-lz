dd9619f46d9e58872ea309fab16a0addf9d29bf2
Pat Heard <patrick.heard@cds-snc.ca>
2023-10-06 19:26:43 +0000

    feat: add SRE Security Hub autmation rule role
    Add an OIDC rule that can be used by the SRE repo by
    the `tools/aws-security-automation-rules` script to manage
    Security Hub automation rules.

--- terragrunt/log_archive/sre_bot/locals.tf
+++ terragrunt/log_archive/sre_bot/locals.tf
@@ -3,5 +3,6 @@ locals {
     CostCentre = var.billing_code
     Terraform  = "true"
   }
-  sre_vulnerability_report_oidc_role = "sre_vulnerability_report_github_action"
+  sre_sechub_automation_rules_oidc_role = "sre_sechub_automation_rules_github_action"
+  sre_vulnerability_report_oidc_role    = "sre_vulnerability_report_github_action"
 }
