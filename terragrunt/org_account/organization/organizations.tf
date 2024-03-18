9059094bbf5c50d7e0667f1b5f5c074369bc234b
Guillaume Charest <guillaume.charest@cds-snc.ca>
2024-03-18 16:13:52 -0400

    fix: attach univ guardrails to Security OU

--- terragrunt/org_account/organization/organizations.tf
+++ terragrunt/org_account/organization/organizations.tf
@@ -64,6 +64,10 @@ resource "aws_organizations_organizational_unit" "Security" {
   parent_id = local.root
 }
 
+resource "aws_organizations_policy_attachment" "Security-cds_snc_universal_guardrails" {
+  policy_id = aws_organizations_policy.cds_snc_universal_guardrails.id
+  target_id = aws_organizations_organizational_unit.Security.id
+}
 
 resource "aws_organizations_organizational_unit" "SRETools" {
   name      = "SRETools"
