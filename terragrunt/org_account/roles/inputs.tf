5dea3ad9793fdc6dae2364e8975148ec5dfd8cc6
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2024-03-28 12:20:16 +0000

    fix: add inputs

--- terragrunt/org_account/roles/inputs.tf
+++ terragrunt/org_account/roles/inputs.tf
@@ -3,3 +3,18 @@ variable "admin_sso_role_arn" {
   description = "(Required) The ARN for the admin SSO role"
   sensitive   = true
 }
+
+variable "account_id" {
+  type        = string
+  description = "(Required) The account ID for the account"
+}
+
+variable "region" {
+  type        = string
+  description = "(Required) The region for the account"
+}
+
+variable "billing_code" {
+  type        = string
+  description = "(Required) The billing code for the account"
+}
