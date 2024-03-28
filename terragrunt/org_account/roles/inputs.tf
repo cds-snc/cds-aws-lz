9ea93b53feee7980caa8482303be1f01434334b3
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2024-03-28 12:29:29 +0000

    fix: remove duplicate vars

--- terragrunt/org_account/roles/inputs.tf
+++ terragrunt/org_account/roles/inputs.tf
@@ -4,12 +4,3 @@ variable "admin_sso_role_arn" {
   sensitive   = true
 }
 
-variable "account_id" {
-  type        = string
-  description = "(Required) The account ID for the account"
-}
-
-variable "region" {
-  type        = string
-  description = "(Required) The region for the account"
-}
