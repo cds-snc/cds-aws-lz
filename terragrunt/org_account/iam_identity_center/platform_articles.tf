839db7fbc4d5764862c31d0cb37657ba2ac8c54c
Pat Heard <patrick.heard@cds-snc.ca>
2024-04-10 12:22:11 +0000

    feat: create end-state group structure
    Remove the existing group imports and instead create the
    target end-state groups and permission assignments.

--- terragrunt/org_account/iam_identity_center/platform_articles.tf
+++ terragrunt/org_account/iam_identity_center/platform_articles.tf
@@ -1,15 +1,39 @@
 #
 # Groups
 #
-resource "aws_identitystore_group" "articles_devs" {
-  display_name      = "GCArticlesDevs"
-  description       = "Grants members access to the GC Articles accounts."
+resource "aws_identitystore_group" "articles_production_access_vpc_clientvpn" {
+  display_name      = "Articles-Production-Access-VPC-ClientVPN"
+  description       = "Grants members access to the GC Articles Production Client VPN."
   identity_store_id = local.sso_identity_store_id
 }
 
-resource "aws_identitystore_group" "articles_vpn" {
-  display_name      = "GCArticlesVPN"
-  description       = "Grants members access to the GC Articles VPN."
+resource "aws_identitystore_group" "articles_production_admin" {
+  display_name      = "Articles-Production-Admin"
+  description       = "Grants members administrator access to the GC Articles Production account."
+  identity_store_id = local.sso_identity_store_id
+}
+
+resource "aws_identitystore_group" "articles_production_read_only" {
+  display_name      = "Articles-Production-ReadOnly"
+  description       = "Grants members read-only access to the GC Articles Production account."
+  identity_store_id = local.sso_identity_store_id
+}
+
+resource "aws_identitystore_group" "articles_staging_access_vpc_clientvpn" {
+  display_name      = "Articles-Staging-Access-VPC-ClientVPN"
+  description       = "Grants members access to the GC Articles Staging Client VPN."
+  identity_store_id = local.sso_identity_store_id
+}
+
+resource "aws_identitystore_group" "articles_staging_admin" {
+  display_name      = "Articles-Staging-Admin"
+  description       = "Grants members administrator access to the GC Articles Staging account."
+  identity_store_id = local.sso_identity_store_id
+}
+
+resource "aws_identitystore_group" "articles_staging_read_only" {
+  display_name      = "Articles-Staging-ReadOnly"
+  description       = "Grants members read-only access to the GC Articles Staging account."
   identity_store_id = local.sso_identity_store_id
 }
 
@@ -18,95 +42,51 @@ resource "aws_identitystore_group" "articles_vpn" {
 #
 locals {
   articles_permission_set_arns = [
+    # GCArticles-Production
     {
-      name = "AWSAdministratorAccess",
-      arn  = data.aws_ssoadmin_permission_set.aws_administrator_access.arn,
+      target_id          = "472286471787"
+      permission_set_arn = data.aws_ssoadmin_permission_set.aws_administrator_access.arn,
+      principal_id       = aws_identitystore_group.articles_production_admin.group_id,
     },
     {
-      name = "AWSReadOnlyAccess",
-      arn  = data.aws_ssoadmin_permission_set.aws_read_only_access.arn,
+      target_id          = "472286471787"
+      permission_set_arn = data.aws_ssoadmin_permission_set.aws_read_only_access.arn,
+      principal_id       = aws_identitystore_group.articles_production_read_only.group_id,
+    },
+    # GCArticles-Staging       
+    {
+      target_id          = "729164266357"
+      principal_id       = aws_identitystore_group.articles_staging_admin.group_id,
+      permission_set_arn = data.aws_ssoadmin_permission_set.aws_administrator_access.arn,
+    },
+    {
+      target_id          = "729164266357"
+      permission_set_arn = data.aws_ssoadmin_permission_set.aws_read_only_access.arn,
+      principal_id       = aws_identitystore_group.articles_staging_read_only.group_id,
+    },
+    # PlatformListManager-Production
+    {
+      target_id          = "762579868088"
+      permission_set_arn = data.aws_ssoadmin_permission_set.aws_administrator_access.arn,
+      principal_id       = aws_identitystore_group.articles_production_admin.group_id,
+    },
+    {
+      target_id          = "762579868088"
+      permission_set_arn = data.aws_ssoadmin_permission_set.aws_read_only_access.arn,
+      principal_id       = aws_identitystore_group.articles_production_read_only.group_id,
     },
   ]
 }
 
-resource "aws_ssoadmin_account_assignment" "articles_devs_staging" {
-  for_each = { for perm in local.articles_permission_set_arns : perm.name => perm }
+resource "aws_ssoadmin_account_assignment" "articles" {
+  for_each = { for perm in local.articles_permission_set_arns : perm.principal_id.name => perm }
 
   instance_arn       = local.sso_instance_arn
-  permission_set_arn = each.value.arn
+  permission_set_arn = each.value.permission_set_arn
 
-  principal_id   = aws_identitystore_group.articles_devs.group_id
+  principal_id   = each.value.principal_id
   principal_type = "GROUP"
 
-  target_id   = "729164266357"
+  target_id   = each.value.target_id
   target_type = "AWS_ACCOUNT"
-}
-
-resource "aws_ssoadmin_account_assignment" "articles_devs_production" {
-  for_each = { for perm in local.articles_permission_set_arns : perm.name => perm }
-
-  instance_arn       = local.sso_instance_arn
-  permission_set_arn = each.value.arn
-
-  principal_id   = aws_identitystore_group.articles_devs.group_id
-  principal_type = "GROUP"
-
-  target_id   = "472286471787"
-  target_type = "AWS_ACCOUNT"
-}
-
-resource "aws_ssoadmin_account_assignment" "articles_devs_platform_list_manager" {
-  for_each = { for perm in local.articles_permission_set_arns : perm.name => perm }
-
-  instance_arn       = local.sso_instance_arn
-  permission_set_arn = each.value.arn
-
-  principal_id   = aws_identitystore_group.articles_devs.group_id
-  principal_type = "GROUP"
-
-  target_id   = "762579868088"
-  target_type = "AWS_ACCOUNT"
-}
-
-#
-# Terraform state imports: remove after merge to `main`
-#
-import {
-  to = aws_identitystore_group.articles_devs
-  id = "${local.sso_identity_store_id}/2c2df578-9041-7052-74b1-a2d362f212bb"
-}
-
-import {
-  to = aws_identitystore_group.articles_vpn
-  id = "${local.sso_identity_store_id}/dccd4518-30d1-7014-0e65-d503dc3c4b75"
-}
-
-import {
-  to = aws_ssoadmin_account_assignment.articles_devs_staging["AWSAdministratorAccess"]
-  id = "2c2df578-9041-7052-74b1-a2d362f212bb,GROUP,729164266357,AWS_ACCOUNT,${data.aws_ssoadmin_permission_set.aws_administrator_access.arn},${local.sso_instance_arn}"
-}
-
-import {
-  to = aws_ssoadmin_account_assignment.articles_devs_staging["AWSReadOnlyAccess"]
-  id = "2c2df578-9041-7052-74b1-a2d362f212bb,GROUP,729164266357,AWS_ACCOUNT,${data.aws_ssoadmin_permission_set.aws_read_only_access.arn},${local.sso_instance_arn}"
-}
-
-import {
-  to = aws_ssoadmin_account_assignment.articles_devs_production["AWSAdministratorAccess"]
-  id = "2c2df578-9041-7052-74b1-a2d362f212bb,GROUP,472286471787,AWS_ACCOUNT,${data.aws_ssoadmin_permission_set.aws_administrator_access.arn},${local.sso_instance_arn}"
-}
-
-import {
-  to = aws_ssoadmin_account_assignment.articles_devs_production["AWSReadOnlyAccess"]
-  id = "2c2df578-9041-7052-74b1-a2d362f212bb,GROUP,472286471787,AWS_ACCOUNT,${data.aws_ssoadmin_permission_set.aws_read_only_access.arn},${local.sso_instance_arn}"
-}
-
-import {
-  to = aws_ssoadmin_account_assignment.articles_devs_platform_list_manager["AWSAdministratorAccess"]
-  id = "2c2df578-9041-7052-74b1-a2d362f212bb,GROUP,762579868088,AWS_ACCOUNT,${data.aws_ssoadmin_permission_set.aws_administrator_access.arn},${local.sso_instance_arn}"
-}
-
-import {
-  to = aws_ssoadmin_account_assignment.articles_devs_platform_list_manager["AWSReadOnlyAccess"]
-  id = "2c2df578-9041-7052-74b1-a2d362f212bb,GROUP,762579868088,AWS_ACCOUNT,${data.aws_ssoadmin_permission_set.aws_read_only_access.arn},${local.sso_instance_arn}"
 }
