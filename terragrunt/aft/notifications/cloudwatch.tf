9d5dbc9bed5cae02cb7f507210c8fb6bc863a3f7
Pat Heard <patrick.heard@cds-snc.ca>
2024-04-30 14:05:21 -0400

    fix: fmt and unique metric name

--- terragrunt/aft/notifications/cloudwatch.tf
+++ terragrunt/aft/notifications/cloudwatch.tf
@@ -1,8 +1,8 @@
 locals {
-    aft_pipeline_log_groups = toset([
-        "/aws/codebuild/aft-global-customizations-terraform",
-        "/aws/codebuild/aft-account-customizations-terraform"
-    ])
+  aft_pipeline_log_groups = toset([
+    "/aws/codebuild/aft-global-customizations-terraform",
+    "/aws/codebuild/aft-account-customizations-terraform"
+  ])
 }
 
 resource "aws_cloudwatch_log_metric_filter" "pipeline_failed" {
@@ -13,7 +13,7 @@ resource "aws_cloudwatch_log_metric_filter" "pipeline_failed" {
   log_group_name = each.key
 
   metric_transformation {
-    name          = "AFTPipelineFailed"
+    name          = "PipelineFailed-${element(split("/", each.key), 3)}"
     namespace     = "AFT"
     value         = "1"
     default_value = "0"
@@ -23,7 +23,7 @@ resource "aws_cloudwatch_log_metric_filter" "pipeline_failed" {
 resource "aws_cloudwatch_metric_alarm" "pipeline_failed" {
   for_each = local.aft_pipeline_log_groups
 
-  alarm_name          = "PipelineFailed-${split("/", each.key)[2]}"
+  alarm_name          = "PipelineFailed-${element(split("/", each.key), 3)}"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = "1"
   metric_name         = aws_cloudwatch_log_metric_filter.pipeline_failed[each.key].metric_transformation[0].name
