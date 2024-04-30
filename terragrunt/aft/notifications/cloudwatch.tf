4b9f74a140b9e92094f469b988c9319da86f24cf
Pat Heard <patrick.heard@cds-snc.ca>
2024-04-30 13:26:02 -0400

    feat: add AFT pipeline CloudWatch metric alarm
    Add a CloudWatch metric and alarm to alert when an AFT Terraform
    pipeline execution fails.

---
+++ terragrunt/aft/notifications/cloudwatch.tf
@@ -0,0 +1,39 @@
+locals {
+    aft_pipeline_log_groups = toset([
+        "/aws/codebuild/aft-global-customizations-terraform",
+        "/aws/codebuild/aft-account-customizations-terraform"
+    ])
+}
+
+resource "aws_cloudwatch_log_metric_filter" "pipeline_failed" {
+  for_each = local.aft_pipeline_log_groups
+
+  name           = "PipelineFailed"
+  pattern        = "FAILED"
+  log_group_name = each.key
+
+  metric_transformation {
+    name          = "AFTPipelineFailed"
+    namespace     = "AFT"
+    value         = "1"
+    default_value = "0"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "pipeline_failed" {
+  for_each = local.aft_pipeline_log_groups
+
+  alarm_name          = "PipelineFailed-${split("/", each.key)[2]}"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = aws_cloudwatch_log_metric_filter.pipeline_failed[each.key].metric_transformation[0].name
+  namespace           = aws_cloudwatch_log_metric_filter.pipeline_failed[each.key].metric_transformation[0].namespace
+  period              = "60"
+  statistic           = "Sum"
+  threshold           = 0
+  treat_missing_data  = "notBreaching"
+
+  alarm_description = "AFT pipeline execution has failed"
+  alarm_actions     = [data.aws_sns_topic.aft_failure_notifications.arn]
+  ok_actions        = [data.aws_sns_topic.aft_failure_notifications.arn]
+}
