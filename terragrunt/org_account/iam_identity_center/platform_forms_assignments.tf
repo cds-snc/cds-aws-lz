5445d6446522161f04cf5d8d0868c7580d2c5ee8
Pat Heard <patrick.heard@cds-snc.ca>
2024-05-11 15:14:28 +0000

    fix: pass entire permission set

--- terragrunt/org_account/iam_identity_center/platform_forms_assignments.tf
+++ terragrunt/org_account/iam_identity_center/platform_forms_assignments.tf
@@ -5,36 +5,36 @@ locals {
   # Forms-Production
   forms_production_permission_set_arns = [
     {
-      group              = aws_identitystore_group.forms_production_admin,
-      permission_set_arn = data.aws_ssoadmin_permission_set.aws_administrator_access.arn,
+      group          = aws_identitystore_group.forms_production_admin,
+      permission_set = data.aws_ssoadmin_permission_set.aws_administrator_access,
     },
     {
-      group              = aws_identitystore_group.forms_production_read_only,
-      permission_set_arn = data.aws_ssoadmin_permission_set.aws_read_only_access.arn,
+      group          = aws_identitystore_group.forms_production_read_only,
+      permission_set = data.aws_ssoadmin_permission_set.aws_read_only_access,
     },
   ]
   # Forms-Staging
   forms_staging_permission_set_arns = [
     {
-      group              = aws_identitystore_group.forms_staging_admin,
-      permission_set_arn = data.aws_ssoadmin_permission_set.aws_administrator_access.arn,
+      group          = aws_identitystore_group.forms_staging_admin,
+      permission_set = data.aws_ssoadmin_permission_set.aws_administrator_access,
     },
     {
-      group              = aws_identitystore_group.forms_staging_read_only,
-      permission_set_arn = data.aws_ssoadmin_permission_set.aws_read_only_access.arn,
+      group          = aws_identitystore_group.forms_staging_read_only,
+      permission_set = data.aws_ssoadmin_permission_set.aws_read_only_access,
     },
     {
-      group              = aws_identitystore_group.forms_staging_read_only,
-      permission_set_arn = aws_ssoadmin_permission_set.read_only_billing.arn,
+      group          = aws_identitystore_group.forms_staging_read_only,
+      permission_set = aws_ssoadmin_permission_set.read_only_billing,
     },
   ]
 }
 
 resource "aws_ssoadmin_account_assignment" "forms_production" {
-  for_each = { for perm in local.forms_production_permission_set_arns : perm.group.display_name => perm }
+  for_each = { for perm in local.forms_production_permission_set_arns : "${perm.group.display_name}-${perm.permission_set.name}" => perm }
 
   instance_arn       = local.sso_instance_arn
-  permission_set_arn = each.value.permission_set_arn
+  permission_set_arn = each.value.permission_set.arn
 
   principal_id   = each.value.group.group_id
   principal_type = "GROUP"
@@ -44,10 +44,10 @@ resource "aws_ssoadmin_account_assignment" "forms_production" {
 }
 
 resource "aws_ssoadmin_account_assignment" "forms_staging" {
-  for_each = { for perm in local.forms_staging_permission_set_arns : perm.group.display_name => perm }
+  for_each = { for perm in local.forms_staging_permission_set_arns : "${perm.group.display_name}-${perm.permission_set.name}" => perm }
 
   instance_arn       = local.sso_instance_arn
-  permission_set_arn = each.value.permission_set_arn
+  permission_set_arn = each.value.permission_set.arn
 
   principal_id   = each.value.group.group_id
   principal_type = "GROUP"
