8023a39a86e541123b6aa73fb704d69c60618a41
Pat Heard <patrick.heard@cds-snc.ca>
2024-05-15 11:45:14 -0400

    feat: Digital Credentials, Design System and Website groups (#265)
    Add the groups and account assignments for Digital Credentials
    and Design System.

--- terragrunt/org_account/iam_identity_center/locals.tf
+++ terragrunt/org_account/iam_identity_center/locals.tf
@@ -1,11 +1,20 @@
 locals {
-  articles_production_account_id = "472286471787"
-  articles_staging_account_id    = "729164266357"
+
+  alpha_canada_website_production_account_id      = "414662622316"
+  canadian_digital_services_production_account_id = "866996500832"
+  cds_website_production_account_id               = "521732289257"
+  cds_website_cms_production_account_id           = "773858180673"
+
+  articles_production_account_id     = "472286471787"
+  articles_staging_account_id        = "729164266357"
+  list_manager_production_account_id = "762579868088"
+
+  design_system_production_account_id = "307395567143"
+
+  digital_credentials_dev_account_id = "767397971970"
 
   forms_production_account_id = "957818836222"
   forms_staging_account_id    = "687401027353"
-
-  list_manager_production_account_id = "762579868088"
 
   notify_production_account_id = "296255494825"
   notify_staging_account_id    = "239043911459"
---
+++ terragrunt/org_account/iam_identity_center/platform_cds_website_permissions.tf
@@ -0,0 +1,61 @@
+#
+# Canadian Digital Service website admin
+#
+resource "aws_ssoadmin_permission_set" "canadian_digital_service_production_website_admin" {
+  name         = "CDS-Website-Admin"
+  description  = "Grants admin access to the Canadian Digital Service's account resources that are used by the main CDS website."
+  instance_arn = local.sso_instance_arn
+}
+
+locals {
+  canadian_digital_service_production_website_admin_policy_arns = toset([
+    "arn:aws:iam::aws:policy/AWSCertificateManagerFullAccess",
+    "arn:aws:iam::aws:policy/AWSWAFFullAccess",
+    "arn:aws:iam::aws:policy/CloudFrontFullAccess",
+    "arn:aws:iam::aws:policy/CloudWatchFullAccessV2",
+    "arn:aws:iam::aws:policy/ReadOnlyAccess"
+  ])
+}
+
+resource "aws_ssoadmin_managed_policy_attachment" "canadian_digital_service_production_website_admin" {
+  for_each           = local.canadian_digital_service_production_website_admin_policy_arns
+  instance_arn       = local.sso_instance_arn
+  managed_policy_arn = each.key
+  permission_set_arn = aws_ssoadmin_permission_set.canadian_digital_service_production_website_admin.arn
+}
+
+resource "aws_ssoadmin_permission_set_inline_policy" "canadian_digital_service_production_website_admin" {
+  permission_set_arn = aws_ssoadmin_permission_set.canadian_digital_service_production_website_admin.arn
+  inline_policy      = data.aws_iam_policy_document.canadian_digital_service_production_website_admin.json
+  instance_arn       = local.sso_instance_arn
+}
+
+data "aws_iam_policy_document" "canadian_digital_service_production_website_admin" {
+  statement {
+    sid    = "S3ListBuckets"
+    effect = "Allow"
+    actions = [
+      "s3:ListAllMyBuckets"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+  statement {
+    sid    = "S3ReadWriteWebsiteDistObjects"
+    effect = "Allow"
+    actions = [
+      "s3:Describe*",
+      "s3:DeleteObject*",
+      "s3:Get*",
+      "s3:List*",
+      "s3:PutObject*"
+    ]
+    resources = [
+      "arn:aws:s3:::cds-website-english-dist",
+      "arn:aws:s3:::cds-website-english-dist/*",
+      "arn:aws:s3:::cds-website-french-dist",
+      "arn:aws:s3:::cds-website-french-dist/*"
+    ]
+  }
+}
