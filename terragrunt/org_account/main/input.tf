602349bc6382f26005dc5a60ba35b162113bc252
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2024-12-17 00:51:27 +0000

    Removing a line by mistake

--- terragrunt/org_account/main/input.tf
+++ terragrunt/org_account/main/input.tf
@@ -12,6 +12,7 @@ variable "lw_customer_ids" {
   type        = list(string)
   description = "The log workspace customer ids for the Azure Sentinel role. This allows us to pass in multiple workspaces"
 }
+
 variable "lw_shared_key" {
   type        = string
   description = "The log workspace shared key for the sentinel forwarder"
