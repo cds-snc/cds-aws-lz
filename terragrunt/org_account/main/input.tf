4218e6a387d8d040231f2a861f3401df0a22c20b
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2024-12-17 09:19:33 -0800

    Adding ability to send multiple workspaces to the Azure sentinel role (#325)
    * Adding ability to send multiple workspaces to the Azure sentinel role. This is temporary as we will be switching to only one log analytics workspace
    
    * Fixing readme and fixing issue with sentinel role
    
    * Formatting
    
    * Removing a line by mistake

--- terragrunt/org_account/main/input.tf
+++ terragrunt/org_account/main/input.tf
@@ -8,6 +8,11 @@ variable "lw_customer_id" {
   description = "The log workspace customer id for the sentinel forwarder"
 }
 
+variable "lw_customer_ids" {
+  type        = list(string)
+  description = "The log workspace customer ids for the Azure Sentinel role. This allows us to pass in multiple workspaces"
+}
+
 variable "lw_shared_key" {
   type        = string
   description = "The log workspace shared key for the sentinel forwarder"
