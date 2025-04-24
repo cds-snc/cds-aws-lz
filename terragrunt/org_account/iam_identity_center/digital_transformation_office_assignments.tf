69fee428b5ea20ac8c9db7600f1279ccfb88df07
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2025-04-24 12:35:43 -0700

    Adding CRA dashboard groups (#344)
    * Adding Cra dashboard groups
    
    * Formatting

--- terragrunt/org_account/iam_identity_center/digital_transformation_office_assignments.tf
+++ terragrunt/org_account/iam_identity_center/digital_transformation_office_assignments.tf
@@ -30,6 +30,20 @@ locals {
       permission_set = data.aws_ssoadmin_permission_set.aws_read_only_access,
     },
   ]
+  cra_dashboard_staging_permission_sets = [
+    {
+      group          = aws_identitystore_group.cra_dashboard_staging_admin,
+      permission_set = data.aws_ssoadmin_permission_set.aws_administrator_access,
+    },
+    {
+      group          = aws_identitystore_group.cra_dashboard_staging_billing_read_only,
+      permission_set = aws_ssoadmin_permission_set.read_only_billing,
+    },
+    {
+      group          = aws_identitystore_group.cra_dashboard_staging_read_only,
+      permission_set = data.aws_ssoadmin_permission_set.aws_read_only_access,
+    },
+  ]
 }
 
 resource "aws_ssoadmin_account_assignment" "digital_transformation_office_production" {
@@ -55,5 +69,18 @@ resource "aws_ssoadmin_account_assignment" "digital_transformation_office_stagin
   principal_type = "GROUP"
 
   target_id   = local.digital_transformation_office_staging_account_id
+  target_type = "AWS_ACCOUNT"
+}
+
+resource "aws_ssoadmin_account_assignment" "cra_dashboard_staging" {
+  for_each = { for perm in local.cra_dashboard_staging_permission_sets : "${perm.group.display_name}-${perm.permission_set.name}" => perm }
+
+  instance_arn       = local.sso_instance_arn
+  permission_set_arn = each.value.permission_set.arn
+
+  principal_id   = each.value.group.group_id
+  principal_type = "GROUP"
+
+  target_id   = local.cra_dashboard_staging_account_id
   target_type = "AWS_ACCOUNT"
 }
--- terragrunt/org_account/iam_identity_center/digital_transformation_office_groups.tf
+++ terragrunt/org_account/iam_identity_center/digital_transformation_office_groups.tf
@@ -36,3 +36,22 @@ resource "aws_identitystore_group" "digtal_transformation_office_staging_billing
   description       = "Grants members read-only Billing and Cost Explorer access to the Digital Transformation office Staging account."
   identity_store_id = local.sso_identity_store_id
 }
+
+# 
+# CRA Dashboard Staging 
+#
+resource "aws_identitystore_group" "cra_dashboard_staging_admin" {
+  display_name      = "CRADashboard-Staging-Admin"
+  description       = "Grants members administrator access to the CRA Dashboard Staging account."
+  identity_store_id = local.sso_identity_store_id
+}
+resource "aws_identitystore_group" "cra_dashboard_staging_read_only" {
+  display_name      = "CRADashboard-Staging-ReadOnly"
+  description       = "Grants members read-only access to the CRA Dashboard Staging account."
+  identity_store_id = local.sso_identity_store_id
+}
+resource "aws_identitystore_group" "cra_dashboard_staging_billing_read_only" {
+  display_name      = "CRADashboard-Staging-Billing-ReadOnly"
+  description       = "Grants members read-only Billing and Cost Explorer access to the Digital Transformation office Staging account."
+  identity_store_id = local.sso_identity_store_id
+}
