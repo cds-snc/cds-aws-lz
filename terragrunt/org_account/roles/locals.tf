6454358bb0be9f5b46d6255bd45d58c7e467e705
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2025-06-25 08:51:39 -0700

    Adding the necessary roles in the org account for the CBS roles to be generated (#361)

--- terragrunt/org_account/roles/locals.tf
+++ terragrunt/org_account/roles/locals.tf
@@ -6,4 +6,5 @@ locals {
   org_account_list_name        = "listAccountsInSandboxOUForNuke"
   org_allow_policy_toggle      = "ghActionAllowPolicyToggle"
   sre_identity_audit_oidc_role = "sre_identity_audit_oidc_role"
+  cbs_central_account_id       = "871282759583"
 }
