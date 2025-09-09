8287d05f374becf38e5e7bd25f4c8e6cb8b75116
Adam Andrews <adam.andrews@cds-snc.ca>
2025-09-09 16:29:17 -0400

    Adding a role with organisation:ListAccounts

--- terragrunt/org_account/roles/locals.tf
+++ terragrunt/org_account/roles/locals.tf
@@ -3,7 +3,8 @@ locals {
     CostCentre = var.billing_code
     Terraform  = "true"
   }
-  org_account_list_name        = "listAccountsInSandboxOUForNuke"
+  org_account_list_name        = "listAccountsInSandboxOUForNuke" # restricted to sandbox OU
+  org_account_list_all_name        = "listAllAccounts" # list all accounts in the org
   org_allow_policy_toggle      = "ghActionAllowPolicyToggle"
   sre_identity_audit_oidc_role = "sre_identity_audit_oidc_role"
   cbs_central_account_id       = "871282759583"
