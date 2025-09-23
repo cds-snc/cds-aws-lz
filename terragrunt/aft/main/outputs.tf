8b4ddfd47c842897756886e77a52da87b23b52eb
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2025-09-23 11:36:10 -0400

    Fix/dependency input (#379)
    * fix: filename typo
    
    * fix: remove skip_outputs and simplify slack_notification_lambda_arn assignment

--- terragrunt/aft/main/outputs.tf
+++ terragrunt/aft/main/outputs.tf
@@ -1,4 +1,4 @@
 output "slack_notification_lambda_arn" {
   value       = module.aft_slack_notification.lambda_arn
-  description = "The ARN of the Lambda function that sends notifications to Slack."
+  description = "The ARN of the Lambda function that sends notifications to Slack"
 }
