bdf01a755ddbdaf4d6b6abed3bd9391b8cd3cac0
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2025-10-21 15:09:40 -0400

    feat: create new SCP deny EC2 connect (#384)
    * feat: create new SCP deny EC2 connect
    
    * Update terragrunt/org_account/organization/scp_deny_ec2_connect.tf
    
    Co-authored-by: Pat Heard <patrick.heard@cds-snc.ca>
    
    * fix: add EC2 CreateKeyPair action
    
    * feat/add additional actions to deny policy
    
    * chore: lint
    
    ---------
    
    Co-authored-by: Pat Heard <patrick.heard@cds-snc.ca>

---
+++ terragrunt/org_account/organization/scp_deny_ec2_connect.tf
@@ -0,0 +1,27 @@
+data "aws_iam_policy_document" "scp_deny_ec2_connect" {
+  statement {
+    effect = "Deny"
+    actions = [
+      "ec2:CreateKeyPair",
+      "ec2:DescribeInstances",
+      "ec2-instance-connect:OpenTunnel",
+      "ec2-instance-connect:SendSSHPublicKey",
+      "ec2-instance-connect:SendSerialConsoleSSHPublicKey",
+      "ssm:DescribeSessions",
+      "ssm:GetConnectionStatus",
+      "ssm:StartSession",
+      "ssm:TerminateSession"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+}
+
+
+resource "aws_organizations_policy" "scp_deny_ec2_connect" {
+  name        = "Deny EC2 Instance Connect"
+  description = "Deny EC2 Instance Connect actions across the organization"
+  type        = "SERVICE_CONTROL_POLICY"
+  content     = data.aws_iam_policy_document.scp_deny_ec2_connect.json
+}
