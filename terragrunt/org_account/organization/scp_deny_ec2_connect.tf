71bcb99b62cbb648f76c8a08dd6b81f7dee8aecb
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2025-10-21 15:03:21 -0400

    feat/add additional actions to deny policy

--- terragrunt/org_account/organization/scp_deny_ec2_connect.tf
+++ terragrunt/org_account/organization/scp_deny_ec2_connect.tf
@@ -3,9 +3,14 @@ data "aws_iam_policy_document" "scp_deny_ec2_connect" {
     effect = "Deny"
     actions = [
       "ec2:CreateKeyPair",
+      "ec2:DescribeInstances",
       "ec2-instance-connect:OpenTunnel",
       "ec2-instance-connect:SendSSHPublicKey",
       "ec2-instance-connect:SendSerialConsoleSSHPublicKey"
+      "ssm:DescribeSessions",
+      "ssm:GetConnectionStatus",
+      "ssm:StartSession",
+      "ssm:TerminateSession"
     ]
     resources = [
       "*"
