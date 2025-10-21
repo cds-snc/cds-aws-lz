29af839a89681a67d9826812a2877fe425a60392
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2025-10-21 17:49:22 +0000

    feat: create new SCP deny EC2 connect

---
+++ terragrunt/org_account/organization/scp_deny_ec2_connect.tf
@@ -0,0 +1,20 @@
+data "aws_iam_policy_document" "scp_deny_ec2_connect" {
+  statement {
+    effect = "Deny"
+    actions = [
+      "ec2-instance-connect:SendSSHPublicKey",
+      "ec2-instance-connect:SendSerialConsoleSSHPublicKey"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+}
+
+
+resource "aws_organizations_policy" "scp_deny_ec2_connect" {
+  name        = "Deny EC2 Instance Connect"
+  description = "Deny EC2 Instance Connect actions across the organization"
+  type        = "SERVICE_CONTROL_POLICY"
+  content     = data.aws_iam_policy_document.scp_deny_ec2_connect.json
+}
