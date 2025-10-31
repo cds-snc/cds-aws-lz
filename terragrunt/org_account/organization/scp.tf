03a438f8c28e117ba937757f29ffa30b2b9ad6cf
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2025-10-31 16:11:51 +0000

    feat: update DTO account number

--- terragrunt/org_account/organization/scp.tf
+++ terragrunt/org_account/organization/scp.tf
@@ -30,7 +30,7 @@ data "aws_iam_policy_document" "cds_snc_universal_guardrails" {
         "493890668711", # Wanpeng Yang scratch 
         "009883649233", # Calvin Rodo scratch 
         "412578375350",  # Sylvia McLaughlin scratch 
-        "000000000000" # DTO Tools AI Staging account
+        "144414543732" # DTO Tools AI Staging account
       ]
     }
     condition {
