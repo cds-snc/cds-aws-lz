0a94801e83b0decd702eba6079d3b899bf7568b3
Pat Heard <patrick.heard@cds-snc.ca>
2026-05-05 14:01:54 -0400

    Revert "Adding exception so that Forms production can delete the resource (#409)" (#410)
    This reverts commit 6b0b740c2de642af9f6d0923455376c95057ed68.

--- terragrunt/org_account/organization/scp.tf
+++ terragrunt/org_account/organization/scp.tf
@@ -114,11 +114,6 @@ data "aws_iam_policy_document" "cds_snc_universal_guardrails" {
     resources = [
       "*"
     ]
-    condition {
-      test     = "StringNotEquals"
-      variable = "aws:PrincipalAccount"
-      values   = ["957818836222"]
-    }
   }
 
   statement {
