3d222eb2d92245609bd273b7afa01c7f8f4cd3e3
Pat Heard <patrick.heard@cds-snc.ca>
2026-08-27 09:05:42 -0400

    feat: add client VPN group for Platform Unified Accounts (#454)
    Add a group that will be used to grant access to the Staging
    client VPN.

--- terragrunt/org_account/iam_identity_center/platform_unified_accounts_groups.tf
+++ terragrunt/org_account/iam_identity_center/platform_unified_accounts_groups.tf
@@ -7,6 +7,12 @@ resource "aws_identitystore_group" "unified_accounts_staging_admin" {
   identity_store_id = local.sso_identity_store_id
 }
 
+resource "aws_identitystore_group" "unified_accounts_staging_vpc_clientvpn" {
+  display_name      = "Unified-Accounts-Staging-VPC-ClientVPN-Access"
+  description       = "Grants members access to the Unified Accounts Staging Client VPN."
+  identity_store_id = local.sso_identity_store_id
+}
+
 resource "aws_identitystore_group" "unified_accounts_staging_read_only_billing" {
   display_name      = "Unified-Accounts-Staging-Billing-ReadOnly"
   description       = "Grants members read-only Billing and Cost Explorer access to the Unified Accounts Staging account."
