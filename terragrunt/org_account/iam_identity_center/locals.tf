1be54cad409213913f0c76429019f39fc9db1471
Pat Heard <patrick.heard@cds-snc.ca>
2026-09-02 16:44:52 -0400

    feat: add PlatformUnifiedAcounts prod groups
    Add the permission groups for the production account. This change also duplicates the staging
    accounts with the correct name.
    
    Once the change has applied, the old groups with the `Platform-` prefix will be removed.

--- terragrunt/org_account/iam_identity_center/locals.tf
+++ terragrunt/org_account/iam_identity_center/locals.tf
@@ -44,6 +44,8 @@ locals {
   strategic_data_reporting_production_account_id = "154541629452"
 
   unified_accounts_staging_account_id = "915144821201"
+  platform_unified_accounts_production_account_id = "778127141858"
+  platform_unified_accounts_staging_account_id = "915144821201"
 
   sso_identity_store_id = "d-9d67173bdd"
   sso_instance_id       = "ssoins-8824c710b5ddb452"
