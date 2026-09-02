0fd444604c4c15d06e4c4ede3463c239d4c0af16
Pat Heard <patrick.heard@cds-snc.ca>
2026-09-02 16:55:39 -0400

    feat: add PlatformUnifiedAcounts prod groups (#459)
    Add the permission groups for the production account. This change also duplicates the staging
    accounts with the correct name.
    
    Once the change has applied, the old groups with the `Platform-` prefix will be removed.

--- terragrunt/org_account/iam_identity_center/platform_unified_accounts_assignments.tf
+++ terragrunt/org_account/iam_identity_center/platform_unified_accounts_assignments.tf
@@ -31,3 +31,62 @@ resource "aws_ssoadmin_account_assignment" "unified_accounts_staging" {
   target_id   = local.unified_accounts_staging_account_id
   target_type = "AWS_ACCOUNT"
 }
+
+locals {
+  # PlatformUnifiedAccounts-Production
+  platform_unified_accounts_production_permission_sets = [
+    {
+      group          = aws_identitystore_group.platform_unified_accounts_production_admin,
+      permission_set = data.aws_ssoadmin_permission_set.aws_administrator_access,
+    },
+    {
+      group          = aws_identitystore_group.platform_unified_accounts_production_read_only_billing,
+      permission_set = aws_ssoadmin_permission_set.read_only_billing,
+    },
+    {
+      group          = aws_identitystore_group.platform_unified_accounts_production_read_only,
+      permission_set = data.aws_ssoadmin_permission_set.aws_read_only_access,
+    }
+  ]
+  # PlatformUnifiedAccounts-Staging
+  platform_unified_accounts_staging_permission_sets = [
+    {
+      group          = aws_identitystore_group.platform_unified_accounts_staging_admin,
+      permission_set = data.aws_ssoadmin_permission_set.aws_administrator_access,
+    },
+    {
+      group          = aws_identitystore_group.platform_unified_accounts_staging_read_only_billing,
+      permission_set = aws_ssoadmin_permission_set.read_only_billing,
+    },
+    {
+      group          = aws_identitystore_group.platform_unified_accounts_staging_read_only,
+      permission_set = data.aws_ssoadmin_permission_set.aws_read_only_access,
+    }
+  ]
+}
+
+resource "aws_ssoadmin_account_assignment" "platform_unified_accounts_production" {
+  for_each = { for perm in local.platform_unified_accounts_production_permission_sets : "${perm.group.display_name}-${perm.permission_set.name}" => perm }
+
+  instance_arn       = local.sso_instance_arn
+  permission_set_arn = each.value.permission_set.arn
+
+  principal_id   = each.value.group.group_id
+  principal_type = "GROUP"
+
+  target_id   = local.platform_unified_accounts_production_account_id
+  target_type = "AWS_ACCOUNT"
+}
+
+resource "aws_ssoadmin_account_assignment" "platform_unified_accounts_staging" {
+  for_each = { for perm in local.platform_unified_accounts_staging_permission_sets : "${perm.group.display_name}-${perm.permission_set.name}" => perm }
+
+  instance_arn       = local.sso_instance_arn
+  permission_set_arn = each.value.permission_set.arn
+
+  principal_id   = each.value.group.group_id
+  principal_type = "GROUP"
+
+  target_id   = local.platform_unified_accounts_staging_account_id
+  target_type = "AWS_ACCOUNT"
+}
--- terragrunt/org_account/iam_identity_center/platform_unified_accounts_groups.tf
+++ terragrunt/org_account/iam_identity_center/platform_unified_accounts_groups.tf
@@ -24,3 +24,51 @@ resource "aws_identitystore_group" "unified_accounts_staging_read_only" {
   description       = "Grants members read-only access to the Unified Accounts Staging account."
   identity_store_id = local.sso_identity_store_id
 }
+
+resource "aws_identitystore_group" "platform_unified_accounts_staging_admin" {
+  display_name      = "Platform-Unified-Accounts-Staging-Admin"
+  description       = "Grants members administrator access to the Platform Unified Accounts Staging account."
+  identity_store_id = local.sso_identity_store_id
+}
+
+resource "aws_identitystore_group" "platform_unified_accounts_staging_vpc_clientvpn" {
+  display_name      = "Platform-Unified-Accounts-Staging-VPC-ClientVPN-Access"
+  description       = "Grants members access to the Platform Unified Accounts Staging Client VPN."
+  identity_store_id = local.sso_identity_store_id
+}
+
+resource "aws_identitystore_group" "platform_unified_accounts_staging_read_only_billing" {
+  display_name      = "Platform-Unified-Accounts-Staging-Billing-ReadOnly"
+  description       = "Grants members read-only Billing and Cost Explorer access to the Platform Unified Accounts Staging account."
+  identity_store_id = local.sso_identity_store_id
+}
+
+resource "aws_identitystore_group" "platform_unified_accounts_staging_read_only" {
+  display_name      = "Platform-Unified-Accounts-Staging-ReadOnly"
+  description       = "Grants members read-only access to the Platform Unified Accounts Staging account."
+  identity_store_id = local.sso_identity_store_id
+}
+
+resource "aws_identitystore_group" "platform_unified_accounts_production_admin" {
+  display_name      = "Platform-Unified-Accounts-Production-Admin"
+  description       = "Grants members administrator access to the Platform Unified Accounts Production account."
+  identity_store_id = local.sso_identity_store_id
+}
+
+resource "aws_identitystore_group" "platform_unified_accounts_production_vpc_clientvpn" {
+  display_name      = "Platform-Unified-Accounts-Production-VPC-ClientVPN-Access"
+  description       = "Grants members access to the Platform Unified Accounts Production Client VPN."
+  identity_store_id = local.sso_identity_store_id
+}
+
+resource "aws_identitystore_group" "platform_unified_accounts_production_read_only_billing" {
+  display_name      = "Platform-Unified-Accounts-Production-Billing-ReadOnly"
+  description       = "Grants members read-only Billing and Cost Explorer access to the Platform Unified Accounts Production account."
+  identity_store_id = local.sso_identity_store_id
+}
+
+resource "aws_identitystore_group" "platform_unified_accounts_production_read_only" {
+  display_name      = "Platform-Unified-Accounts-Production-ReadOnly"
+  description       = "Grants members read-only access to the Platform Unified Accounts Production account."
+  identity_store_id = local.sso_identity_store_id
+}
