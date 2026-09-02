a457b27e20d44a69c5eda50b6ce45dcb9db3edd8
Pat Heard <patrick.heard@cds-snc.ca>
2026-09-02 19:23:07 -0400

    feat: add group to provide Athena query access
    Add a group and permission set to Platform Unified Accounts that will
    allow members to perform Athena queries.

--- terragrunt/org_account/iam_identity_center/platform_unified_accounts_groups.tf
+++ terragrunt/org_account/iam_identity_center/platform_unified_accounts_groups.tf
@@ -25,6 +25,13 @@ resource "aws_identitystore_group" "platform_unified_accounts_staging_read_only"
   identity_store_id = local.sso_identity_store_id
 }
 
+resource "aws_identitystore_group" "platform_unified_accounts_staging_athena_query_access" {
+  display_name      = "Platform-Unified-Accounts-Staging-Athena-Query-Access"
+  description       = "Grants members access to run Athena queries in the Platform Unified Accounts Staging account."
+  identity_store_id = local.sso_identity_store_id
+}
+
+
 #
 # Production
 #
@@ -49,5 +56,11 @@ resource "aws_identitystore_group" "platform_unified_accounts_production_read_on
 resource "aws_identitystore_group" "platform_unified_accounts_production_read_only" {
   display_name      = "Platform-Unified-Accounts-Production-ReadOnly"
   description       = "Grants members read-only access to the Platform Unified Accounts Production account."
+  identity_store_id = local.sso_identity_store_id
+}
+
+resource "aws_identitystore_group" "platform_unified_accounts_production_athena_query_access" {
+  display_name      = "Platform-Unified-Accounts-Production-Athena-Query-Access"
+  description       = "Grants members access to run Athena queries in the Platform Unified Accounts Production account."
   identity_store_id = local.sso_identity_store_id
 }
