431cbb25c09e291d42256fc10bc2fd09f830f144
Pat Heard <patrick.heard@cds-snc.ca>
2026-09-03 10:05:06 -0400

    fix: update permission set name (#464)
    Update the Platform Unified Accounts permission set name.

--- terragrunt/org_account/iam_identity_center/platform_unified_accounts_permissions.tf
+++ terragrunt/org_account/iam_identity_center/platform_unified_accounts_permissions.tf
@@ -2,8 +2,8 @@
 # Athena query access
 #
 resource "aws_ssoadmin_permission_set" "platform_unified_accounts_athena_query_access" {
-  name         = "PlatformUnifiedAccounts-Athena" # There is a 32 character limit on permission set names, so this is shortened and it will bother me every time I look at it
-  description  = "Grants access to the Athena query editor."
+  name         = "Athena-Query-Access-PUA"
+  description  = "Grants access to the Athena query editor in Platform Unified Accounts."
   instance_arn = local.sso_instance_arn
 }
 
