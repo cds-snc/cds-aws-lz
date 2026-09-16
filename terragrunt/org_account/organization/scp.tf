15e8f5456ca9c34a5547a2ab1812c1f611180f55
Pat Heard <patrick.heard@cds-snc.ca>
2026-09-16 17:58:04 -0400

    fix: add another hosted zone to the SCP
    Prevent the deletion of another of our subdomain delegated
    hosted zones.

--- terragrunt/org_account/organization/scp.tf
+++ terragrunt/org_account/organization/scp.tf
@@ -177,6 +177,7 @@ data "aws_iam_policy_document" "cds_snc_universal_guardrails" {
       "arn:aws:route53:::hostedzone/Z33C47YI9EN8XL",
       "arn:aws:route53:::hostedzone/Z1XG153PQF3VV5",
       "arn:aws:route53:::hostedzone/Z35N8HLYUZDWBH",
+      "arn:aws:route53:::hostedzone/Z0089505EYRWTLOO204V",
     ]
   }
 }
